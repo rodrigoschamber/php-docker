e118fdb3ee9fd1ddb9f335444823375985d9f346
rodrigoschamber <rodrigoschamber@gmail.com>
2022-01-27 21:42:04 -0300

    Database connection fixed

--- src/includes/db.php
+++ src/includes/db.php
@@ -1,5 +1,15 @@
 <?php
-    $connection = mysqli_connect('localhost', 'root', 'example', 'cms');
+    $db['db_host'] = "db";
+    $db['db_user'] = "root";
+    $db['db_pass'] = "example";
+    $db['db_name'] = "cms";
+
+    foreach($db as $key => $value) {
+        define(strtoupper($key), $value);
+    }
+
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    
     if ($connection) {
         echo "Connected to the database";
     }
